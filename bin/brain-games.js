17f80e402d6c93f24108fda3b4ccabcd84363937
Your Name <you@example.com>
2025-03-03 13:23:51 +0700

    bbb

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import cli from './src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-cli();
+import cli from '../src/cli.js'
+import readlineSync from 'readline-sync';
+import welcome from '../src/cli.js';
+welcome()
