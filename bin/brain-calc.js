6a7e18039aad83bb85c32a75c6ba3322921b780a
Your Name <you@example.com>
2025-03-06 11:36:12 +0700

    calculate

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
+
 const getAnswerOfQuestion = {
-    '+': function (x,y) { return x + y;},
-    '-': function (x,y) { return x - y;},
-    '*': function (x,y) { return x * y;},
+    '+': function (x, y) { return x + y; },
+    '-': function (x, y) { return x - y; },
+    '*': function (x, y) { return x * y; }
 }
 
 export const calculateGame = (name) => {
@@ -16,19 +16,24 @@ export const calculateGame = (name) => {
     let count = 0
     const signs = ['+', '-', '*'];
 
-    while(count < 3) {
+    while (count < 3) {
         const firstNumber = getRandomIntInclusive(25, 55)
         const secondNumber = getRandomIntInclusive(25, 55)
-        const sign = signs[getRandomIntInclusive](0, 2)
+        const sign = signs[getRandomIntInclusive(0, 2)]
 
-        const answerOfQuestion = getAnswerOfQuestion[sign] (firstNumber, secondNumber)
+        const answerOfQuestion = getAnswerOfQuestion[sign](firstNumber, secondNumber)
         console.log(`Question : ${firstNumber} ${sign} ${secondNumber}`)
-        const userAnswer = rreadlineSync.question(`Your answer:`)
-        if (userAnswear === answerOfQuestion) {
-    count++
-    console.log('Correct!')
+        const userAnswer = readlineSync.question(`Your answer:`)
+        if (userAnswer == answerOfQuestion) {
+            count++
+            console.log('Correct!')
+        } else {
+            console.log(`'${userAnswer}'  is wrong answer ;(. Correct answer was '${answerOfQuestion}'.\nLet's try again, Sam!`)
+            return
         }
+       
     }
+     console.log(`Congratulations, Sam!`)
 }
+calculateGame()
 
- 
