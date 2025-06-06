43259a108718eb851d4b042b8fbd22e1a8b52f7f
Your Name <you@example.com>
2025-06-06 19:36:38 +0700

    fix brain-prime

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,21 @@ export default function welcome(description) {
     console.log('Welcome to the Brain Games!')
     const name = readlineSync.question('May I have your name? ')
     console.log('Hello, ' + name + '!')
-    if (description != undefined){
+    if (description != undefined) {
         console.log(description)
     }
     return name
 }
 export const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+
+export const isPrime = (num) => {
+    for(let i = 2, s= Math.sqrt(num); i <= s; i += 1) {
+        if(num % i === 0) {
+            return false
+        }
+        return num > 1
+    }
+}
 
 export function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
@@ -18,9 +27,9 @@ export function getRandomIntInclusive(min, max) {
 }
 
 export const getAnswerOfQuestion = {
-    '+': function (x,y) { return x + y;},
-    '-': function (x,y) { return x - y;},
-    '*': function (x,y) { return x * y;},
+    '+': function (x, y) { return x + y; },
+    '-': function (x, y) { return x - y; },
+    '*': function (x, y) { return x * y; },
 }
 
 
@@ -29,37 +38,37 @@ export const calculateGame = (name) => {
     let count = 0
     const signs = ['+', '-', '*'];
 
-    while(count < 3) {
+    while (count < 3) {
         const firstNumber = getRandomIntInclusive(25, 55)
         const secondNumber = getRandomIntInclusive(25, 55)
         const sign = signs[getRandomIntInclusive](0, 2)
 
-        const answerOfQuestion = getAnswerOfQuestion[sign] (firstNumber, secondNumber)
+        const answerOfQuestion = getAnswerOfQuestion[sign](firstNumber, secondNumber)
         console.log(`Question : ${firstNumber} ${sign} ${secondNumber}`)
         const userAnswer = rreadlineSync.question(`Your answer:`)
         if (userAnswear === answerOfQuestion) {
-    count++
-    console.log('Correct!')
+            count++
+            console.log('Correct!')
         }
     }
 }
 
-export function progression () {
-const arr = [];
+export function progression() {
+    const arr = [];
 
-const start =  getRandomIntInclusive(1, 10);
-const length =  getRandomIntInclusive(5, 10);
-const step =  getRandomIntInclusive(1, 20);
+    const start = getRandomIntInclusive(1, 10);
+    const length = getRandomIntInclusive(5, 10);
+    const step = getRandomIntInclusive(1, 20);
 
-for (let i = 0; i < length; i += 1) {
-  arr.push(start + step * i);
-}
+    for (let i = 0; i < length; i += 1) {
+        arr.push(start + step * i);
+    }
 
-const randIndex =  getRandomIntInclusive(0, arr.length - 1);
-const correctNumber = arr[randIndex];
-arr[randIndex] = '..';
+    const randIndex = getRandomIntInclusive(0, arr.length - 1);
+    const correctNumber = arr[randIndex];
+    arr[randIndex] = '..';
 
-return { arr, correctNumber };
+    return { arr, correctNumber };
 }
 
 
